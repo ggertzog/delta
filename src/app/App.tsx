7ed3116e062f0db3c1a59764b2f066db1720d4f8
ggertzog <musienko.roma@mail.ru>
2024-09-18 16:11:57 +0300

    transfer a project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,8 @@
+import { AboutPage } from '@/pages/about';
+import { HomePage } from '@/pages/home-page';
+import { ProfilePage } from '@/pages/profile';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 /**
  * Renders the main application component.
@@ -6,7 +10,13 @@ import React from 'react';
  * @return {ReactElement} The rendered application component.
  */
 const App: React.FC = () => {
-  return <>App</>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  );
 };
 
 export default App;
